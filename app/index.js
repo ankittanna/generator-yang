9a4d032d2e6cdf967e6e85e57b4a003591b6532b
Ankit <ankit.bharat.tanna@accenture.com>
2016-03-17 01:25:41 +0530

    Yeoman: Public Private Method
    use _ for private

--- app/index.js
+++ app/index.js
@@ -9,12 +9,22 @@ var _ = require('lodash');
 	}
 }); */
 
-module.exports = generators.NamedBase.extend({
+/* module.exports = generators.NamedBase.extend({
 	constructor: function(){
 		generators.NamedBase.apply(this, arguments);
 		this.log('name ', this.name);
 	},
 	method1: function(){
 		console.log("Hello World!");
+	}
+}); */
+
+// Works by Order
+module.exports = generators.Base.extend({
+	_foo: function(){
+		console.log("Inside Foo");
+	},
+	bar: function(){
+		console.log("Inside Bar");
 	}
 });
